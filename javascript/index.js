1cb67463b75d402a4c2042a275a4a25456fad125
Alan Romero <emanuromet@gmail.com>
2023-10-26 18:20:58 -0300

    add code

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+
+
+
+let btnRegistrar = document.getElementById('btnRegistrar')
+let inputSolicitud = document.getElementById('inputSolicitud');
+
+btnRegistrar.addEventListener("click", (e) => {
+    e.preventDefault()
+    alert("Holas")
+})
