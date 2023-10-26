ece69a67efe9ac4a873debaa6436ef49d1d82415
Alan Romero <emanuromet@gmail.com>
2023-10-26 18:44:25 -0300

    add code

--- javascript/index.js
+++ javascript/index.js
@@ -3,8 +3,16 @@
 
 let btnRegistrar = document.getElementById('btnRegistrar')
 let inputSolicitud = document.getElementById('inputSolicitud');
+let inputArea = document.getElementById('inputArea')
+let inputNombre = document.getElementById('inputNombre')
+let inputTelefono = document.getElementById('inputTelefono')
+let inputDescripcion = document.getElementById('inputDescripcion')
 
 btnRegistrar.addEventListener("click", (e) => {
     e.preventDefault()
-    alert("Holas")
+    let solicitud = inputSolicitud.value;
+    let area = inputArea.value;
+    let nombre = inputNombre.value;
+    let telefono = inputTelefono.value;
+    let descripcion = inputDescripcion.value;
 })
